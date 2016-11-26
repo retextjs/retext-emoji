f5fb40e752c4b2cb00721cbb66d276f4e0e3220b
Titus Wormer <tituswormer@gmail.com>
2016-11-26 17:35:52 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module retext-emoji
- * @fileoverview Emoji, gemoji, and emoticons with retext.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/* Dependencies. */
 var affixEmoticonModifier = require('nlcst-affix-emoticon-modifier');
 var emoticonModifier = require('nlcst-emoticon-modifier');
 var emojiModifier = require('nlcst-emoji-modifier');
@@ -19,127 +8,117 @@ var toString = require('nlcst-to-string');
 var gemoji = require('gemoji');
 var visit = require('unist-util-visit');
 
-/* Constants. */
+module.exports = emoji;
+
 var EMOTICON_NODE = 'EmoticonNode';
 
 /* Map of visitors. */
 var fns = {
-    encode: toEmoji,
-    decode: toGemoji
-}
+  encode: toEmoji,
+  decode: toGemoji
+};
 
-/* Easy access. */
 var unicodes = gemoji.unicode;
 var names = gemoji.name;
 
 var shortcodes = {};
 
-emoticons = emoticons.emoticon;
-
 (function () {
-    var key;
-    var shortcode;
+  var key;
+  var shortcode;
+  var result = {};
+  var length = emoticons.length;
+  var index = -1;
+  var count;
+  var offset;
+  var subset;
+  var name;
 
-    for (key in names) {
-        shortcode = ':' + key + ':';
-        shortcodes[shortcode] = names[key];
-        shortcodes[shortcode].shortcode = shortcode;
-    }
+  for (key in names) {
+    shortcode = ':' + key + ':';
+    shortcodes[shortcode] = names[key];
+    shortcodes[shortcode].shortcode = shortcode;
+  }
 
-    for (key in emoticons) {
-        emoticons[key].names = names[emoticons[key].name].names;
-        emoticons[key].shortcode = names[emoticons[key].name].shortcode;
+  while (++index < length) {
+    name = emoticons[index].name;
+    subset = emoticons[index].emoticons;
+    count = subset.length;
+    offset = -1;
+
+    while (++offset < count) {
+      result[subset[offset]] = names[name];
     }
+  }
+
+  emoticons = result;
 })();
 
-/**
- * Replace a short-code with a unicode emoji.
- *
- * @param {EmoticonNode} node - Emoticon node.
- */
-function toEmoji(node) {
-    var value = toString(node);
-    var info = (shortcodes[value] || emoticons[value] || {}).emoji;
-
-    if (info) {
-        node.value = info;
-    }
-}
-
-/**
- * Replace a unicode emoji with a short-code.
- *
- * @param {EmoticonNode} node - Emoticon node.
- */
-function toGemoji(node) {
-    var value = toString(node);
-    var info = (unicodes[value] || emoticons[value] || {}).shortcode;
-
-    if (info) {
-        node.value = info;
-    }
-}
-
-/**
- * Partially applied visit factory.
- *
- * @param {Function?} transformer - EmoticonNode-visitor.
- * @return {Function?} - Vititor.
- */
-function visitFactory(transformer) {
-    return function (node) {
-        visit(node, EMOTICON_NODE, function (node) {
-            var data = node.data;
-            var value = toString(node);
-            var info;
-
-            if (transformer) {
-                transformer(node);
-            }
-
-            info = unicodes[value] || shortcodes[value] || emoticons[value];
-
-            if (!data) {
-                data = node.data = {};
-            }
-
-            data.names = info.names.concat();
-            data.description = info.description;
-            data.tags = info.tags.concat();
-        });
-    }
-}
-
-/**
- * Attacher.
- *
- * @param {Retext} processor - Instance.
- * @param {Object?} [options] - Configuration.
- * @return {Function} Transformer.
- */
+/* Attacher. */
 function emoji(processor, options) {
-    var proto = processor.Parser.prototype;
-    var convert = (options || {}).convert;
-    var fn;
+  var proto = processor.Parser.prototype;
+  var convert = (options || {}).convert;
+  var fn;
 
-    proto.useFirst('tokenizeSentence', emoticonModifier);
-    proto.useFirst('tokenizeSentence', emojiModifier);
-    proto.useFirst('tokenizeParagraph', affixEmoticonModifier);
+  proto.useFirst('tokenizeSentence', emoticonModifier);
+  proto.useFirst('tokenizeSentence', emojiModifier);
+  proto.useFirst('tokenizeParagraph', affixEmoticonModifier);
 
-    if (convert !== null && convert !== undefined) {
-        fn = fns[convert];
+  if (convert !== null && convert !== undefined) {
+    fn = fns[convert];
 
-        if (!fn) {
-            throw new TypeError(
-                'Illegal invocation: `' + convert +
-                '` is not a valid value for ' +
-                '`options.convert` in `retext#use(emoji, options)`'
-            );
-        }
+    if (!fn) {
+      throw new TypeError(
+        'Illegal invocation: `' + convert +
+        '` is not a valid value for ' +
+        '`options.convert` in `retext#use(emoji, options)`'
+      );
+    }
+  }
+
+  return transformer;
+
+  function transformer(node) {
+    visit(node, EMOTICON_NODE, visitor);
+  }
+
+  function visitor(node) {
+    var data = node.data;
+    var value = toString(node);
+    var info;
+
+    if (fn) {
+      fn(node);
     }
 
-    return visitFactory(fn);
+    info = unicodes[value] || shortcodes[value] || emoticons[value];
+
+    if (!data) {
+      data = node.data = {};
+    }
+
+    data.names = info.names.concat();
+    data.description = info.description;
+    data.tags = info.tags.concat();
+  }
 }
 
-/* Expose. */
-module.exports = emoji;
+/* Replace a unicode emoji with a short-code. */
+function toGemoji(node) {
+  var value = toString(node);
+  var info = (unicodes[value] || emoticons[value] || {}).shortcode;
+
+  if (info) {
+    node.value = info;
+  }
+}
+
+/* Replace a short-code with a unicode emoji. */
+function toEmoji(node) {
+  var value = toString(node);
+  var info = (shortcodes[value] || emoticons[value] || {}).emoji;
+
+  if (info) {
+    node.value = info;
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,129 +1,111 @@
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module nlcst:to-string
- * @fileoverview Test suite for `retext-emoji`.
- */
-
 'use strict';
 
-/* eslint-env node */
-/* jscs:disable jsDoc */
-/* jscs:disable maximumLineLength */
-
-/* Dependencies. */
 var test = require('tape');
 var retext = require('retext');
 var visit = require('unist-util-visit');
 var emoji = require('..');
 
-/* Fixtures. */
 var emoticon = require('./fixture/emoticon');
 var emojis = require('./fixture/emoji');
 var gemoji = require('./fixture/gemoji');
 
-/* Tests. */
 test('toString()', function (t) {
-    t.test('should throw when given invalid `convert`', function (st) {
-        st.throws(function () {
-            retext().use(emoji, {
-                convert: false
-            });
-        }, /Illegal invocation: `false` is not a valid value/);
+  t.test('should throw when given invalid `convert`', function (st) {
+    st.throws(
+      function () {
+        retext().use(emoji, {convert: false});
+      },
+      /Illegal invocation: `false` is not a valid value/
+    );
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should classify emoticons', function (st) {
-        var processor = retext().use(emoji);
-        var tree = processor.parse('This makes me feel :).')
+  t.test('should classify emoticons', function (st) {
+    var processor = retext().use(emoji);
+    var tree = processor.parse('This makes me feel :).');
 
-        processor.run(tree);
+    processor.run(tree);
 
-        st.deepEqual(tree, emoticon);
+    st.deepEqual(tree, emoticon);
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should classify gemoji', function (st) {
-        var processor = retext().use(emoji);
-        var tree = processor.parse('This makes me feel :sob:.')
+  t.test('should classify gemoji', function (st) {
+    var processor = retext().use(emoji);
+    var tree = processor.parse('This makes me feel :sob:.');
 
-        processor.run(tree);
+    processor.run(tree);
 
-        st.deepEqual(tree, gemoji);
+    st.deepEqual(tree, gemoji);
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should classify emoji', function (st) {
-        var processor = retext().use(emoji);
-        var tree = processor.parse('It’s raining 🐱s and 🐶s.');
+  t.test('should classify emoji', function (st) {
+    var processor = retext().use(emoji);
+    var tree = processor.parse('It’s raining 🐱s and 🐶s.');
 
-        processor.run(tree);
+    processor.run(tree);
 
-        st.deepEqual(tree, emojis);
+    st.deepEqual(tree, emojis);
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should not transform without `convert`', function (st) {
-        var processor = retext().use(emoji);
-        var input = 'It’s raining 🐱s and :dog:s. Now :3.';
-        var output = processor.process(input).toString();
+  t.test('should not transform without `convert`', function (st) {
+    var processor = retext().use(emoji);
+    var input = 'It’s raining 🐱s and :dog:s. Now :3.';
+    var output = processor.process(input).toString();
 
-        st.equal(output, input);
+    st.equal(output, input);
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should encode', function (st) {
-        var processor = retext().use(emoji, {convert: 'encode'});
+  t.test('should encode', function (st) {
+    var processor = retext().use(emoji, {convert: 'encode'});
 
-        st.equal(
-            processor.process(
-                'It’s raining 🐱s and :dog:s. Now :3.'
-            ).toString(),
-            'It’s raining 🐱s and 🐶s. Now 👨.'
-        );
+    st.equal(
+      processor.process('It’s raining 🐱s and :dog:s. Now :3.').toString(),
+      'It’s raining 🐱s and 🐶s. Now 👨.'
+    );
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should decode', function (st) {
-        var processor = retext().use(emoji, {convert: 'decode'});
+  t.test('should decode', function (st) {
+    var processor = retext().use(emoji, {convert: 'decode'});
 
-        st.equal(
-            processor.process(
-                'It’s raining 🐱s and :dog:s. Now :3.'
-            ).toString(),
-            'It’s raining :cat:s and :dog:s. Now :man:.'
-        );
+    st.equal(
+      processor.process('It’s raining 🐱s and :dog:s. Now :3.').toString(),
+      'It’s raining :cat:s and :dog:s. Now :man:.'
+    );
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.test('should not overwrite existing data', function (st) {
-        var processor = retext()
-            .use(function () {
-                return function (node) {
-                    visit(node, function (child) {
-                        child.data = {};
-                    });
-                };
-            })
-            .use(emoji);
+  t.test('should not overwrite existing data', function (st) {
+    var processor = retext()
+      .use(function () {
+        return transformer;
+        function transformer(node) {
+          visit(node, visitor);
+        }
+        function visitor(child) {
+          child.data = {};
+        }
+      })
+      .use(emoji);
 
-        st.equal(
-            processor.process(
-                'It’s raining 🐱s and :dog:s. Now :3.'
-            ).toString(),
-            'It’s raining 🐱s and :dog:s. Now :3.'
-        );
+    st.equal(
+      processor.process('It’s raining 🐱s and :dog:s. Now :3.').toString(),
+      'It’s raining 🐱s and :dog:s. Now :3.'
+    );
 
-        st.end();
-    });
+    st.end();
+  });
 
-    t.end();
+  t.end();
 });
