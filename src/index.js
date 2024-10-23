f7dd7ddcc6df4efa44345a72b7f09a86905d0ac4
Titus Wormer <tituswormer@gmail.com>
2024-10-23 17:46:42 +0200

    Rewrite project

--- src/index.js
+++ src/index.js
@@ -1,42 +1,61 @@
-'use strict'
-
 /* eslint-env browser */
 
-var unified = require('unified')
-var english = require('retext-english')
-var stringify = require('retext-stringify')
-var emoji = require('retext-emoji')
+/**
+ * @import {Root} from 'nlcst'
+ * @import {Options} from 'retext-emoji'
+ * @import {Processor} from 'unified'
+ */
 
-var $convert = document.getElementsByName('convert')[0]
-var $areas = document.querySelectorAll('textarea')
+import {ok as assert} from 'devlop'
+import retextEmoji from 'retext-emoji'
+import retextEnglish from 'retext-english'
+import retextStringify from 'retext-stringify'
+import {unified} from 'unified'
 
-var $input = $areas[0]
-var $output = $areas[1]
+const $convert = document.getElementsByName('convert')[0]
+const $areas = document.querySelectorAll('textarea')
 
-var processor
-var state = {options: {}}
+const $input = $areas[0]
+const $output = $areas[1]
+
+/** @type {Processor<Root, Root, undefined, Root, string> | undefined} */
+let processor
+
+/** @type {{options: Options}} */
+const state = {options: {}}
 
 $convert.addEventListener('change', onchange)
-
 $input.addEventListener('input', oninput)
 
 onchange({target: $convert})
 
+/**
+ * @param {{target: EventTarget | null}} event
+ * @returns {undefined}
+ */
 function onchange(event) {
-  var value = event.target.selectedOptions[0]
+  // This is all just because TS doesn’t understand normal code.
+  const target = /** @type {HTMLSelectElement} */ (event.target)
 
-  if (value) {
-    state.options[event.target.name] = value.value
+  const selectedOption = target.selectedOptions[0]
+
+  if (selectedOption) {
+    assert(target.name === 'convert')
+    assert(
+      selectedOption.value === 'decode' || selectedOption.value === 'encode'
+    )
+    state.options[target.name] = selectedOption.value
   }
 
   processor = unified()
-    .use(english)
-    .use(emoji, state.options)
-    .use(stringify)
+    .use(retextEnglish)
+    .use(retextEmoji, state.options)
+    .use(retextStringify)
 
   oninput()
 }
 
 function oninput() {
+  assert(processor)
   $output.value = processor.processSync($input.value).toString()
 }
